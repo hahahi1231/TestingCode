e29955a64f406501dd2a0ab945fd06b8c0f9fd33
hahahi1231 <62507278+hahahi1231@users.noreply.github.com>
2020-03-24 22:22:14 +0700

    Update ChiaDeTriBaiMau.cpp

--- ChiaDeTriBaiMau.cpp
+++ ChiaDeTriBaiMau.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
 //tao mang random
 void sinh(int *&a, int &n){
 	cout<<"\n so phan tu mang can sinh:"; cin>>n;
@@ -12,24 +10,26 @@ void sinh(int *&a, int &n){
 // in ra mang da tao
 void in(int *&a, int &n){
 	cout<<"\n noi dung mang:\n";
-	for(int i=0; i<n; i++) cout<<a[i] <<" ";
+	for(int i=0; i<n; i++) 
+		cout<<a[i] <<" ";
 }
-
+// Sap xep
 void Merge(int*&a, int L, int R){
-	        int m= (L+R)/2; // chia mang thanh 2 phan
+// L, R la bien trai va bien phai cua mang
+	    int m= (L+R)/2; // chia mang thanh 2 phan
 			int i=L, j=m+1, v=0;  
             int *kq= new int[R-L+1]; // mang ket qua tam chua day tang dan
             
              while (i<=m && j<=R)
                 if(a[i]<=a[j]) { 
-					kq[v]=a[i];
-					i++;
+				kq[v]=a[i]; // Neu a[i] < a[j] thi copy phan tu ben trai vao mang tam
+					i++; // Vi tri phan tu tiep theo cua mang
 					v++;
-				 }  
-				else {
+				}  
+			  else { // Neu a[i] > a[j] thi copy phan tu ben phai vao mang tam
 				 kq[v] =a[j]; 
-				 v++; 
-				 j++;
+					 v++; 
+					 j++; // Vi tri phan tu tiep theo cua mang
 				}
 				
              while (i<=m) { 
@@ -37,6 +37,7 @@ void Merge(int*&a, int L, int R){
 					 i++; 
 					 v++;
 			 }
+	     // Nguoc lai dua cac phan tu con lai cua mang con ben phai vao mang tam
              while (j<=R ) { 
 			 	kq[v] =a[j]; 
 					 v++; 
@@ -47,18 +48,16 @@ void Merge(int*&a, int L, int R){
 			 		a[i++] = kq[j];
         
 }
-
 void Mergesort(int*&a, int L, int R){
 	if(L<R)
 	{
 		int m=(L+R)/2;
-		Mergesort(a,  L, m);
-		Mergesort(a,  m+1, R);
-		Merge(a, L, R);
+		Mergesort(a,  L, m); // Goi de quy mang con ben trai
+		Mergesort(a,  m+1, R);// Goi de quy mang con ben phai
+		Merge(a, L, R);// Goi ham so sanh hai mang con
 	}
 
 }
-
 int main(){
 	int *a, n;
 	sinh(a,n);
@@ -66,6 +65,5 @@ int main(){
 	Mergesort(a, 0, n-1);
 	cout<<"\n day sau khi sap xep:\n";
 	in(a,n);
-	
 }
 
